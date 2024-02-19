7155c99f994b457adba1bf060d0a8b292161f442
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-02-19 14:58:48 +0900

    [정렬, 맵, 셋] 2월 19일 - ing

---
+++ 01_정렬_맵_셋/필수/1431.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	string input;
+
+	cin >> n;
+
+	vector<string> arr(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[n];
+
+	}
+}
