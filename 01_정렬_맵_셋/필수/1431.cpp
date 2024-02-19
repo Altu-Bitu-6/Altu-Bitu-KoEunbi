3fecbd4af8c4284758a7c3f502101f43dd12346f
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-02-19 20:21:48 +0900

    [정렬] 2월 19일

--- 01_정렬_맵_셋/필수/1431.cpp
+++ 01_정렬_맵_셋/필수/1431.cpp
@@ -4,16 +4,46 @@
 
 using namespace std;
 
+bool cmpAdv(string& s1, string& s2) {
+	int num1 = 0, num2 = 0;
+
+	if (s1.size() == s2.size()) {
+		for (int i = 0; i < s1.size(); i++) {
+			if (s1[i] >= '0' && s1[i] <= '9') {
+				num1 += s1[i] - '0';
+			}
+			if (s2[i] >= '0' && s2[i] <= '9') {
+				num2 += s2[i] - '0';
+			}
+		}
+		if (num1 == num2) {
+			for (int i = 0; i < s1.size(); i++) {
+				if (s1[i] == s2[i])
+					continue;
+				else
+					return s1[i] < s2[i];
+			}
+		}
+		return num1 < num2;
+	}
+	return s1.size() < s2.size();
+}
+
 int main() {
 	int n;
-	string input;
-
 	cin >> n;
 
-	vector<string> arr(n, 0);
+	string arr[n];
 
 	for (int i = 0; i < n; i++) {
-		cin >> arr[n];
-
+		cin >> arr[i];
 	}
+
+	sort(arr, arr + n, cmpAdv);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << '\n';
+	}
+
+	return 0;
 }
