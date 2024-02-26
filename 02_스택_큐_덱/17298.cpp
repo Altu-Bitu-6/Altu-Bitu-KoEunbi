369dad983c46ff418f5b46f6eeed22a7543f0658
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-02-26 17:37:23 +0900

    [스택, 큐, 덱] 2월 26일

--- 02_스택_큐_덱/17298.cpp
+++ 02_스택_큐_덱/17298.cpp
@@ -4,6 +4,32 @@
 using namespace std;
 
 int main() {
+	int n;
+	stack <int> s;
+
+	cin >> n;
+
+	int arr[n];
+	int result[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = n-1; i >= 0; i--) {
+		while (!s.empty() && s.top() <= arr[i]) {
+			s.pop();
+		}
+		if (s.empty())
+			result[i] = -1;
+		else
+			result[i] = s.top();
+
+		s.push(arr[i]);
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << result[i] << " ";
 
 	return 0;
 }
