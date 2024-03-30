57ad1f49207053f23e0deaad0043a714a3bcfc8a
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-03-30 13:01:52 +0900

    [07 동적계획법] 3월 30일 - ing

---
+++ 07_동적계획법/필수/2579.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, idx = 0, h;
+    vector <int> score;
+    vector <int> hop;
+
+    cin >> n;
+    while (n--) {
+        hop[idx] = 0;
+        cin >> score[idx++];
+    }
+    hop[n - 1] = 1;
+
+    idx = 0;
+    while (1) {
+
+    }
+
+    if (n % 2 == 0) {
+
+
+    }
+    else {
+
+    }
+
+
+
+    return 0;
+}
