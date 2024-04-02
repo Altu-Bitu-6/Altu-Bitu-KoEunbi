3b4659202a808aa096051322b5353087f388cfe8
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-04-02 13:04:37 +0900

    [07 동적계획법] 4월 2일 - ing

--- 07_동적계획법/필수/2579.cpp
+++ 07_동적계획법/필수/2579.cpp
@@ -3,32 +3,42 @@
 
 using namespace std;
 
+int cmp(int a, int b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
 int main()
 {
-    int n, idx = 0, h;
-    vector <int> score;
-    vector <int> hop;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, i;
+    bool flag = true;
 
     cin >> n;
-    while (n--) {
-        hop[idx] = 0;
-        cin >> score[idx++];
-    }
-    hop[n - 1] = 1;
+    vector <int> arr(n);
+    vector <int> score(n);
 
-    idx = 0;
-    while (1) {
 
+    for (i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    if (n % 2 == 0) {
+    score[0] = arr[0];
 
-
+    if (n > 1) {
+        score[1] = arr[0] + arr[1];
     }
-    else {
-
+    if (n > 2) {
+        score[2] = cmp(arr[0] + arr[2], arr[1] + arr[2]);
+    }
+    for (i = 3; i < n; i++) {
+        score[i] = cmp(score[i - 2] + arr[i], score[i - 3] + arr[i - 1] + arr[i]);
     }
 
+    cout << score[n - 1];
 
 
     return 0;
