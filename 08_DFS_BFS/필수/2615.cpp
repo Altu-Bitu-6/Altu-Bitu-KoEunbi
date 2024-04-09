9e601ba7235155e3639fa5f385a9656454a3118a
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2024-04-09 14:35:29 +0900

    [08 DFS BFS] 4월 9일

--- 08_DFS_BFS/필수/2615.cpp
+++ 08_DFS_BFS/필수/2615.cpp
@@ -4,50 +4,93 @@
 
 using namespace std;
 
-const int N = 19;
-int flag = 0;
+const int MAX = 21;
+int x2, y2, mx[] = { -1, 0, 1,1 }, my[] = { 1,1,1,0 };
 vector<vector<int>> map;
-vector<vector<int>> li_b;
-vector<vector<int>> li_w;
-vector <bool> visited_dfs;
+vector <vector<bool>> visited_map;
 
-void dfs(int idx) {
-    visited_dfs[idx] = true;
+void bfs(int x, int y, int x_start, int y_start, int n, int dir) {
+    visited_map[x][y] = true;
 
-    for (int i = 1; i <= N; i++) {
-        if (li[idx][i] == 1 && !visited_dfs[i]) {
-            dfs(i);
+    for (int i = 0; i < 4; i++) {
+        x2 = x + mx[i];
+        y2 = y + my[i];
+        if (x2 >= 1 && x2 <= 19 && y2 >= 1 && y2 <= 19 && !visited_map[x2][y2] && map[x][y] == map[x2][y2]) {
+
+            if (x == x2) {
+                if (dir == 1)
+                    bfs(x2, y2, x_start, y_start, n + 1, dir);
+                else
+                    bfs(x2, y2, x, y, 2, 1);
+            }
+            else if (y == y2) {
+                if (dir == 2)
+                    bfs(x2, y2, x_start, y_start, n + 1, dir);
+                else
+                    bfs(x2, y2, x, y, 2, 2);
+            }
+            else if (x - x2 == 1 && y2 - y == 1) {
+                if (dir == 3)
+                    bfs(x2, y2, x_start, y_start, n + 1, dir);
+                else
+                    bfs(x2, y2, x, y, 2, 3);
+            }
+            else {
+                if (dir == 4)
+                    bfs(x2, y2, x_start, y_start, n + 1, dir);
+                else
+                    bfs(x2, y2, x, y, 2, 4);
+            }
         }
     }
-}
 
+
+    visited_map[x][y] = false;
+    if (n == 5) {
+        if (dir == 1) {
+            if ((y > 5 && map[x][y] == map[x][y - 5]) || (y < 19 && map[x][y] == map[x][y + 1]))
+                return;
+        }
+        else if (dir == 2) {
+            if ((x > 5 && map[x][y] == map[x - 5][y]) || (x < 19 && map[x][y] == map[x + 1][y]))
+                return;
+        }
+        else if (dir == 3) {
+            if ((x < 15 && y>5 && map[x][y] == map[x + 5][y - 5]) || (x > 1 && y < 19 && map[x][y] == map[x - 1][y + 1]))
+                return;
+        }
+        else if (dir == 4) {
+            if ((x > 5 && y > 5 && map[x][y] == map[x - 5][y - 5]) || (x < 19 && y < 19 && map[x][y] == map[x + 1][y + 1]))
+                return;
+        }
+
+        cout << map[x][y] << "\n" << x_start << " " << y_start;
+        exit(0);
+    }//오목
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int i, j, idx_b = 0, idx_w = 0;
+    map.assign(MAX, vector<int>(MAX, 0));
+    visited_map.assign(MAX, vector<bool>(MAX, false));
 
-    map.assign(N, vector<int>(N, 0));
-    li_b.assign(N * N, vector<int>(N, vector<int>(N, 0)));
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            cin >> m;
-            map[i][j] = m;
-
-            if (m == 1)
-                li_b[idx_b][i][j] = li_b[idx_b++][j][i] = 1;
-            else if (m == 2)
-                li_w[idx_w][i][j] = li_w[idx_w++][j][i] = 1;
+    for (int i = 1; i <= 19; i++) {
+        for (int j = 1; j <= 19; j++) {
+            cin >> map[i][j];
         }
-    } //바둑판 입력 받기
+    }
+    //map
 
+    for (int i = 1; i <= 19; i++) {
+        for (int j = 1; j <= 19; j++) {
+            if ((map[i][j] == 1 || map[i][j] == 2) && !visited_map[i][j])
+                bfs(i, j, i, j, 1, 0);
+        }
+    }
 
-    li_w.assign(white, vector<int>(white, 0));
-
-
-
+    cout << 0;
 
     return 0;
 }
