1cb7eff863b0dd8a74b1cbf61becf0425710ff56
Eunbi <121873609+eeeunbiiii@users.noreply.github.com>
2024-04-16 15:38:44 +0900

    [09백트래킹] 4월 16일

--- 09_백트래킹/필수/15665.cpp
+++ 09_백트래킹/필수/15665.cpp
@@ -21,8 +21,8 @@ void dfs(int cnt) {
 
     for (int i = 0; i < num.size(); i++) {
         v.push_back(num[i]);
-        dfs(cnt + 1);
-        v.pop_back();
+        dfs(cnt + 1); //다음 숫자 선택
+        v.pop_back(); //선택한 숫자 제거 
     }
 }
 
@@ -31,13 +31,13 @@ int main() {
 
     int input;
     for (int i = 0; i < n; i++) {
-        cin >> input;
+        cin >> input; //처음 등장하는 숫자만 저장, 방문 기록
         if (visited[input] == 0)
             num.push_back(input);
         visited[input]++;
     }
 
-    sort(num.begin(), num.end());
+    sort(num.begin(), num.end());//오름차순 정렬
 
     for (int i = 0; i < num.size(); i++) {
         v.push_back(num[i]);
