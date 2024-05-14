15393f8c051df3995db2c3a1b13bf92d1ad3ba24
Eunbi <121873609+eeeunbiiii@users.noreply.github.com>
2024-05-14 14:27:17 +0900

    [11 투포인터] 5월 14일

--- 11_투포인터/필수/14503.cpp
+++ 11_투포인터/필수/14503.cpp
@@ -15,15 +15,15 @@ void dfs(int x, int y, int d) {
 		ans++;
 	}
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) {//옆 4방향
 		nd = (d + 3 - i) % 4;
 		nx = x + xx[nd];
 		ny = y + yy[nd];
 
-		if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+		if (nx < 0 || ny < 0 || nx >= n || ny >= m) //맵 범위 외의 위치
 			continue;
 
-		if (map[nx][ny] == 0)
+		if (map[nx][ny] == 0) //청소
 			dfs(nx, ny, nd);
 	}
 
@@ -36,7 +36,7 @@ void dfs(int x, int y, int d) {
 		exit(0);
 	}
 
-	dfs(nx, ny, d);
+	dfs(nx, ny, d); //방향 유지
 }
 
 int main() {
@@ -51,7 +51,7 @@ int main() {
 		for (int j = 0; j < m; j++) {
 			cin >> map[i][j];
 		}
-	}
+	} //map 입력받기
 
 	dfs(r, c, d);
 
